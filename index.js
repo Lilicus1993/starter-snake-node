05bccd0af2857b8fb9908742d71a5fb57b874120
Lilicus1993 <60452313+Lilicus1993@users.noreply.github.com>
2020-01-30 14:27:57 -0800

    let's see

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/start', (request, response) => {
 app.post('/move', (request, response) => {
   // NOTE: Do something here to generate your move
   const arrMove = ["up", "down", "left", "right"];
-  const num = Math.random();
+  const num = Math.random(3);
   let turn = console.log(arrMove[num]);
       
   // Response data
